7ef40e0e5f91295f5a9227141299fdba84abad1d
japaodsc <buigiapao@gmail.com>
2020-08-17 20:52:41 +0700

    fix ip

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const port = 2211;
-const ip = 	"52.221.244.41";
 const server = express();
 const cdata = require('./modules/getcdata');
 const path = require('path');
@@ -17,6 +16,6 @@ server.get("/api/moh/data", (req, res) => {
 server.get('*', function(req, res) {
      res.redirect('/');
  });
- server.listen(port, ip, () => {
+ server.listen(port, '0.0.0.0', () => {
      console.log("RESTful API Server by JaPao. Server is now running at " + port)
  });
