bf54e5e483561b4ac45c12a30f2f0ce799ca250c
japaodsc <buigiapao@gmail.com>
2020-11-04 18:55:57 +0700

    2020 usa election

---
+++ modules/election.js
@@ -0,0 +1,55 @@
+const request = require("request-promise");
+const fetch = require("node-fetch")
+const cheerio = require("cheerio");
+const fs = require("fs");
+const url = "https://www.foxnews.com/elections/2020/general-results";
+const _under = require("underscore");
+const https = require('https');
+const { get } = require("request-promise");
+const cron = require('node-cron')
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+const options = {
+  method: 'GET',
+  agent: httpsAgent,
+  headers: {
+    "Content-Length": 0
+  }
+}
+
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+  setTimeout(resolve, ms);
+ });
+ }
+module.exports= async function() {
+  let now = new Date();
+  let time = now.toLocaleTimeString();
+  let data = await Promise.all([
+    fetch(url, options).then((res) => res.text())
+  ])
+
+    const $ = cheerio.load(data[0]);
+    
+    //VOTES//
+    let bidenElect = $('div.party-label>div.info>div.count').eq(0).text()
+    let trumpElect = $('div.party-label>div.info>div.count').eq(1).text()
+    let bidenVotes =$('div.bar-wrap.has-label-upper.has-label-lower>div.bar-label.is-lower>div.label.label-left>div.popular-vote.dem>span.count').text()
+    let trumpVotes =$('div.bar-wrap.has-label-upper.has-label-lower>div.bar-label.is-lower>div.label.label-right>div.popular-vote.rep>span.count').text()
+
+    let election = {
+      "time" : time,
+       "info": {
+        "bidenElect": bidenElect,  
+        "trumpElect": trumpElect,
+        "bidenVotes": bidenVotes,
+        "trumpVotes": trumpVotes
+       }
+
+      }
+      console.log(election)
+  return election;
+  
+    }
